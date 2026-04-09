c49868e9e534f62b933fbc3f6ee4ce763ccdf6b9
Predrag Buncic <pbuncic@yahoo.com>
2026-04-09 23:44:38 +0200

    Adding common.bits as dependency

--- lhcb.bits.sh
+++ lhcb.bits.sh
@@ -3,5 +3,7 @@ version: "1"
 tag: "main"
 provides_repository: true
 source: https://github.com/bitsorg/lhcb.bits
+requires:
+  - common.bits
 ---
 
--- stacks.bits.sh
+++ stacks.bits.sh
@@ -3,5 +3,7 @@ version: "109"
 tag: "main"
 provides_repository: true
 source: https://github.com/bitsorg/stacks.bits
+requires:
+  - common.bits
 ---
 
