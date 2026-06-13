dbb8cacc910fbbab3986d07de23e8ad847d486e7
Predrag Buncic <pbuncic@yahoo.com>
2026-06-13 13:06:52 +0200

    always_load would prevent loading specific version of repos, removing again

--- alidist.bits.sh
+++ alidist.bits.sh
@@ -2,7 +2,6 @@ package: alidist.bits
 version: "1"
 tag: "master"
 provides_repository: true
-always_load: true
 source: https://github.com/alisw/alidist
 ---
 
--- lcg.bits.sh
+++ lcg.bits.sh
@@ -2,7 +2,6 @@ package: lcg.bits
 version: "1"
 tag: "main"
 provides_repository: true
-always_load: true
 source: https://github.com/bitsorg/lcg.bits
 ---
 
