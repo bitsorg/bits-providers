a2701eedccaebbc6a9f372a5e7e7d2053bd3b702
Predrag Buncic <pbuncic@yahoo.com>
2026-06-23 14:26:36 +0200

    adding always_load: true

--- alidist.bits.sh
+++ alidist.bits.sh
@@ -2,6 +2,7 @@ package: alidist.bits
 version: "1"
 tag: "master"
 provides_repository: true
+always_load: true
 source: https://github.com/alisw/alidist
 ---
 
--- cms.bits.sh
+++ cms.bits.sh
@@ -2,6 +2,7 @@ package: cms.bits
 version: "1"
 tag: "main"
 provides_repository: true
+always_load: true
 source: https://github.com/bitsorg/cms.bits
 ---
 
